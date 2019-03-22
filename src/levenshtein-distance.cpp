b6397612c5eac0c74a53d1186761e37055f03455
heyitshenrylin <qlin1@ualberta.ca>
2019-03-21 20:50:12 -0600

    Initial templates

---
+++ src/levenshtein-distance.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Eric Claerhout, Henry Lin
+Student ID: 1532360, 1580649
+CMPUT 275 Winter 2019
+
+Final Project: Starbucks Simulator
+#########################################
+*/
+#include "levenshtein-distance.h"
+
+int main() {
+
+    return 0;
+}
