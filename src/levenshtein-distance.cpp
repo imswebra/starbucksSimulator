b7ec5b053a4f4e469940d365423ca333005602af
heyitshenrylin <qlin1@ualberta.ca>
2019-03-28 00:05:28 -0600

    1-based and 0-based indexing fix
    Added a secondary

--- src/levenshtein-distance.cpp
+++ src/levenshtein-distance.cpp
@@ -16,23 +16,23 @@ using namespace std;
 // Optimal String Alignment Distance calculation
 // Based off pseudocode found here:
 // https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
-unsigned int levenshtein(string A, string B) {
+unsigned int oStringAlignment(string A, string B) {
     bool cost = 0;  // cost will only be 0 or 1
     unsigned int distance[A.length() + 2][B.length() + 2];
 
-    for(unsigned int i = 0; i <= A.length() + 1; i++) {
+    for (unsigned int i = 0; i <= A.length(); i++) {
         distance[i][0] = i;
     }
-    for(unsigned int j = 0; j <= B.length() + 1; j++) {
+    for (unsigned int j = 0; j <= B.length(); j++) {
         distance[0][j] = j;
     }
 
-    for(unsigned int i = 1; i <= A.length() + 1; i++) {
-        for(unsigned int j = 1; j <= B.length() + 1; j++) {
-            if(A[i] == B[j]) {
+    for (unsigned int i = 1; i <= A.length(); i++) {
+        for (unsigned int j = 1; j <= B.length(); j++) {
+            if (A[i-1] == B[j-1]) {
                 cost = 0;
             }
-            else{
+            else {
                 cost = 1;
             }
             distance[i][j] = min(distance[i - 1][j] + 1,  // deletion
@@ -44,12 +44,11 @@ unsigned int levenshtein(string A, string B) {
             }
         }
     }
-    return distance[A.length() + 1][B.length() + 1];
+    return distance[A.length()][B.length()];
 }
-
 int main() {
-    string s1 = "Damerau";
-    string s2 = "Levenshtein";
-    cout << levenshtein(s1, s2) << endl;
+    string s1 = "CA";
+    string s2 = "ABC";
+    cout << oStringAlignment(s1, s2) << endl;
     return 0;
 }
