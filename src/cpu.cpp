b5d5d7fff94eba1a18a3c4d096b558a7f8c5d57b
heyitshenrylin <qlin1@ualberta.ca>
2019-03-28 18:50:33 -0600

    cpu templates

---
+++ src/cpu.cpp
@@ -0,0 +1,11 @@
+/*
+Author: Eric Claerhout, Henry Lin
+Student ID: 1532360, 1580649
+CMPUT 275 Winter 2019
+
+Final Project: Starbucks Simulator
+#########################################
+*/
+#include "cpu.h"
+
+using namespace std;
---
+++ src/cpu.h
@@ -0,0 +1,14 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+/*
+Author: Eric Claerhout, Henry Lin
+Student ID: 1532360, 1580649
+CMPUT 275 Winter 2019
+
+Final Project: Starbucks Simulator
+#########################################
+*/
+
+
+
+#endif
