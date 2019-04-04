5324ac026db17962d00be2caba6632136c273257
heyitshenrylin <qlin1@ualberta.ca>
2019-04-04 03:05:57 -0600

    Revert "Alphabet indexing fix"
    This reverts commit ac4acde0f317518a64f11f31f188f0c0c965d2b9.

--- src/algs/levenshtein-distance.cpp
+++ src/algs/levenshtein-distance.cpp
@@ -12,12 +12,9 @@ Final Project: Starbucks Simulator
 #include <algorithm>  // for min()
 #include <cstring>  // for memset()
 
-#include "../preprocess.h"
-
-
+#include "preprocess.h"
 
 using namespace std;
-
 
 // Size 26 means that no accented characters are supported
 #define alphabetSize 26  // Supported alphabet size
@@ -41,9 +38,7 @@ int dynamicDL(string& A, string& B) {
 
     // Create the language size table
     unsigned int da[alphabetSize];
-    for(int i = 0; i < alphabetSize; i++) {
-        da[i] = 0;
-    }
+    memset(da, 0, sizeof(da));
 
     // Allocate memory for distance table:
     // https://stackoverflow.com/questions/9219712/c-array-expression-must-have-a-constant-value
@@ -68,7 +63,7 @@ int dynamicDL(string& A, string& B) {
     for (int i = 2; i < aSize + 2; i++) {
         db = 1;
         for (int j = 2; j < bSize + 2; j++) {
-            k = da[B[j-2] - 65] + 1;
+            k = da[B[j-2] - 64] + 1;
             l = db;
             if (A[i-2] == B[j-2]) {
                 cost = 0;
@@ -83,7 +78,7 @@ int dynamicDL(string& A, string& B) {
                                             distances[k-1][l-1] + (i-1-k) + 1
                                                 + (j-1-l))));  // transposition
         }
-        da[A[i-2] - 65] = i;
+        da[A[i-2] - 64] = i;
     }
 
     // The final distance is the last index in the 2-d array
